7ed87aeff0c9724c16621b03c9d705126cca54fd
Nathan Hwang <beta.entity.k@gmail.com>
2011-11-18 21:18:31 -0500

    Weak todo

--- src/robot/PathPlanner.java
+++ src/robot/PathPlanner.java
@@ -1,5 +1,11 @@
 package robot;
 
 public class PathPlanner {
-
+	public static void main(String [] args) {
+		// grow obstacles
+		
+		// create visibility graph
+		
+		// use dijkstra's to get shortest path
+	} 
 }
