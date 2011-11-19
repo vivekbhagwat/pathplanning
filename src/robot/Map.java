85d2e97e24b3ab4085f0be570eaa783614b4d06a
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-19 17:42:17 -0500

    not much implementation

--- src/robot/Map.java
+++ src/robot/Map.java
@@ -2,8 +2,15 @@ package robot;
 
 public class Map
 {
+	public final double ROBOT_SIZE = 1.0; //random value?
+	private Point[][] map;
 	
-	
+	public Map(String inputFile)
+	{
+		//read file here
+		
+		
+	}
 	
 	
 }
