6a77b34ad38c9a62f63ebae972b90c6ab1b6f789
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-20 11:56:49 -0500

    This won't work yet, but prelim implementation of dijkstra's

--- src/robot/PathPlanner.java
+++ src/robot/PathPlanner.java
@@ -1,8 +1,62 @@
 package robot;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 public class PathPlanner {
 	
+	public LinkedList<Point> dijkstra(Map map)
+	{
+		LinkedList<Point> path = new LinkedList<Point>();
+		ArrayList<Point> points = map.map;
+		map.start.dist = 0.0;
+		path.add(map.start);		
+		
+		PriorityQueue<Point> pq = new PriorityQueue<Point>(points.size());
+		
+		for(int i = 0; i < points.size(); i++)
+		{
+			pq.add(points.get(i));
+		}
+		
+		Point current;
+		while((current = pq.poll()) != null)
+		{
+			ArrayList<Point> next = possibleNextPoints(current, map);
+			Point adj = null;
+			for(int i = 0; i < next.size(); i++)
+			{
+				adj = next.get(i);
+				if(!adj.known)
+				{
+					//wrong
+					if(current.dist + map.adjacencyMatrix[i][i] < adj.dist)
+					{
+						//wrong
+						adj.dist = current.dist + map.adjacencyMatrix[i][i];
+						
+						adj.path = current;
+					}
+					pq.add(adj);
+				}
+			}
+		}
+		Point end = map.goal
+		double pathLength = end.dist;
+		while(end.path != null)
+		{
+			path.add(end)
+			end = end.path;
+		}
+		path.add(map.start);
+		return path;		
+	}
 	
+	private ArrayList<Point> possibleNextPoints(Point current, Map map)
+	{
+		//to be implemented
+	}
 	
 	public static void main(String [] args) {
 		// open the map 
