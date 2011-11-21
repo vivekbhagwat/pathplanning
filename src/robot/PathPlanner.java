6969c94c1c716bbd7b5781b6169592fa4664ec8d
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-20 21:52:53 -0500

    fixed bug with test

--- src/robot/PathPlanner.java
+++ src/robot/PathPlanner.java
@@ -68,7 +68,7 @@ public class PathPlanner {
 		pp.add(new Point(1,-1));
 		
 		// open the map 
-		Map map = new Map(args[0]);
+		Map map = new Map(args[0], new Point(-1.0,-1.0), new Point(2.0,2.0));
 		PathPlanner planner = new PathPlanner();
 		planner.dijkstra(map);
 		// grow obstacles
