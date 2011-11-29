04c8924b678b99c8c610a753a952f94f58e32190
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-28 23:26:39 -0500

    nathan awesomeified this

--- followpath.m
+++ followpath.m
@@ -11,8 +11,8 @@ if isSimulator(serPort)
     speed = 0.2;
     turn_speed = 0.2;
 else
-    speed = 0.05;
-    turn_speed = 0.05;
+    speed = 0.2;
+    turn_speed = 0.04;
 end
 
 % we assume that the robot is initially facing the +x direction
@@ -20,7 +20,7 @@ x = 0;
 y = 0;
 angle = 0;
 dist_thresh = speed;
-angle_thresh = 0.05;
+angle_thresh = 0.1;
 % which index we're using as the goal
 index = 2;
 
@@ -29,30 +29,59 @@ while(index <= length(px))
     goal_angle = atan2(py(index)-y, px(index)-x);
     disp(goal_angle);
     AngleSensorRoomba(serPort);
-    while(abs(goal_angle - angle) > angle_thresh)
-        turnAngle(serPort, turn_speed, goal_angle-angle);
-        dangle = AngleSensorRoomba(serPort);
-        angle = angle + dangle;
+    
+    if (goal_angle - angle < 0 ) % Makes sure the robot turns in the right direction
+        turnDir = -eps;
+    else
+        turnDir = eps;
     end
+    downsteps = -10000;
+    SetFwdVelRadiusRoomba(serPort, turn_speed, turnDir);
+    while(abs(goal_angle - angle) > angle_thresh)
+        %turnAngle(serPort, turn_speed, 180*(goal_angle-angle)/pi);
+        dangle = AngleSensorRoomba(serPort);
+        if ~isnan(dangle)
+            angle = angle + dangle;
+            logdown = log2(abs(goal_angle - angle)/angle_thresh);
+            % downsteps monotonically increases (exponent of fraction)
+            if(2-logdown >= downsteps)
+                downsteps = 2 - logdown;
+            end
+            if(downsteps > 0)
+                SetFwdVelRadiusRoomba(serPort, turn_speed*0.7^downsteps, turnDir);
+            end
+        else
+            disp('angle NaN')
+        end
+        disp(angle);
+    end
+    SetFwdVelRadiusRoomba(serPort, 0, inf);
     
     % move towards the next goal
     downsteps = -10000;
     SetFwdVelRadiusRoomba(serPort, speed, inf);
     while(dist([x,y],[px(index),py(index)]) > dist_thresh)
         d = DistanceSensorRoomba(serPort);
-        x = x + d*cos(angle);
-        y = y + d*sin(angle);
-        logdown = log2(dist([x,y],[px(index),py(index)])/dist_thresh);
-        % downsteps monotonically increases (exponent of fraction)
-        if(2-logdown >= downsteps)
-            downsteps = 2 - logdown;
+        if ~isnan(d)
+            x = x + d*cos(angle);
+            y = y + d*sin(angle);
+            logdown = log2(dist([x,y],[px(index),py(index)])/dist_thresh);
+            % downsteps monotonically increases (exponent of fraction)
+            if(2-logdown >= downsteps)
+                downsteps = 2 - logdown;
+            end
+            % disp(downsteps);
+            % disp([x,y]);
+            % disp([px(index),py(index)]);
+            if(downsteps > 0)
+                SetFwdVelRadiusRoomba(serPort, speed*0.7^downsteps, inf);
+            end
+        else
+            disp('dist NaN')
         end
-        % disp(downsteps);
-        % disp([x,y]);
-        % disp([px(index),py(index)]);
-        if(downsteps > 0)
-            SetFwdVelRadiusRoomba(serPort, speed*0.7^downsteps, inf);
-        end
+        disp(d);
+        disp(angle);
+        
         if isSimulator(serPort)
             pause(0.005);
         end
